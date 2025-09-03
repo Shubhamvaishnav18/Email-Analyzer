de7d96d9f540b6df59f722ae31f12a8dba4853a8
Shubham Vaishnav <159063010+Shubhamvaishnav18@users.noreply.github.com>
2025-09-03 14:15:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,11 @@
-const app = require("./app");
-module.exports = (req, res) => {
-  app(req, res);
-};
+const app = require('./app');
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
